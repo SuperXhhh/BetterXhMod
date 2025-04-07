792f7266cff06092b81d7df43af0d3212cc5c41d
Xh <ksxiaohang@foxmail.com>
2025-04-07 15:01:19 +0800

    Initial commit

---
+++ src/main/java/com/xiaohang/betterxhmod/BetterXHModClient.java
@@ -0,0 +1,10 @@
+package com.xiaohang.betterxhmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterXHModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/xiaohang/betterxhmod/BetterXHModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.xiaohang.betterxhmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterXHModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
